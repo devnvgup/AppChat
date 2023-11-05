2e1abeab60252bea84063f49dc05a80991500756
devnvgup <truongnguyen16999@gmail.com>
2023-11-05 10:34:22 +0700

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import "./App.css";
-import ChatRoom from "./components/ChatRoom";
-import Login from "./components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./Context/AuthProvider";
-import AppProvider from "./Context/AppProvider";
-import AddRoomModal from "./components/Modals/AddRoomModal";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <AppProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/chatroom" element={<ChatRoom />} />
-        </Routes>
-        <AddRoomModal/>
-        </AppProvider>
-      </AuthProvider>
-    </Router>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
